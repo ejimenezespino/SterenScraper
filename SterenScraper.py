6df0482b84f70555e4e7100c4d11b3301aaf4162
Eliana Jimenez <ejimenezespinosa@hotmail.com>
2021-10-10 20:25:25 -0500

    small change

--- SterenScraper.py
+++ SterenScraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import csv
-from csv import reader
 
 URL = 'https://www.steren.com.mx/'
 header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
